89f07e5446797e118cba9fbf7eec4e7e9590bb02
Seth MacPherson <macpherson.seth@gmail.com>
2020-10-01 00:13:30 -0700

    A bit more work, but not a lot of progress.

--- js/index.js
+++ js/index.js
@@ -1,49 +1,53 @@
 const siteContent = {
-  "nav": {
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
+    "img-src": "img/logo.png",
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
+    "img-src": "img/header-img.png",
   },
   "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-h4": "Features",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-h4": "Services",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+  contact: {
+    "contact-h4": "Contact",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io",
   },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+  footer: {
+    copyright: "Copyright Great Idea! 2018",
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
 //brought over code from the accidentally unforked copy I was working on.
 //brought over code from the accidentally unforked copy I was working on.
 //brought over code from the accidentally unforked copy I was working on.
-
 
 // const titleChange = document.getElementsByTagName("title");
 // titleChange.textContent = "Great Idea!";
@@ -65,22 +69,22 @@ buttonChange[0].textContent = "Get Started";
 const newCirclePic = document.getElementById("cta-img");
 newCirclePic.setAttribute("src", siteContent["cta"]["img-src"]);
 
-const mainTopText = document.get;
+const newMiddleImg = document.getElementById("middle-img");
+newMiddleImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
 
-
-
-const h4Change = document.getElementsByClassName("text-content");
-console.log("text-content");
-h4Change[0].textContent = siteContent["cta"]["main-content"][0];
+// Not Sure where this code is off yet, but its making divs instead of h4s
+const newMainTopText = document.getElementsByClassName("text-content");
+newMainTopText[0].textContent = siteContent["main-content"]["features-h4"];
+newMainTopText[1].textContent = siteContent["main-content"]["about-h4"];
+newMainTopText[2].textContent = siteContent["main-content"]["services-h4"];
+newMainTopText[3].textContent = siteContent["main-content"]["product-h4"];
+newMainTopText[4].textContent = siteContent["main-content"]["vision-h4"];
 
 //broken code
 
 // h4Change.forEach((ele)=>{
 //   ele[i].innerHTML = siteContent["cta"]["main-content"][i+2]
-// })
-
-// const bigChange = document.getElementsByClassName("text-content");
-// bigChange.forEach((item)=>{
-//   item.h4.textContent = siteContent["cta"]["main-content"][2];
-
-// })
+// // })
+// const h4Change = document.getElementsByClassName("text-content");
+// console.log("text-content");
+// h4Change[0].textContent = siteContent["cta"]["main-content"][0];
